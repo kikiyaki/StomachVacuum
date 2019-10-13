2a1c87cc069b0bb154b98241943b2893f8a026b9
kikiyaki <krl9628@gmail.com>
2019-10-13 17:05:54 +0300

    Просто запуск уведомления сразу, без ресивера

--- app/src/main/java/com/sportsandhealth/iamkerel/stomachvacuum/Entry/ScreenSlidePagerActivity.java
+++ app/src/main/java/com/sportsandhealth/iamkerel/stomachvacuum/Entry/ScreenSlidePagerActivity.java
@@ -12,6 +12,7 @@ import com.sportsandhealth.iamkerel.stomachvacuum.AndroidDatabaseManager;
 import com.sportsandhealth.iamkerel.stomachvacuum.MainActivity;
 import com.sportsandhealth.iamkerel.stomachvacuum.ProgramSelection;
 import com.sportsandhealth.iamkerel.stomachvacuum.R;
+import com.sportsandhealth.iamkerel.stomachvacuum.lib.MyNotification;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -71,6 +72,11 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
                 }
             }
         });
+
+        // Регистрируем канал уведомлений
+        MyNotification myNotification = new MyNotification(this);
+        myNotification.createNotificationChannel();
+
     }
 
     @Override
