d5d8ca9dbcf5ab9e526ba6029c8d2572a63e4981
kikiyaki <krl9628@gmail.com>
2019-10-26 15:48:15 +0300

    Revert "Верстка уведомлений плюс переходы"
    This reverts commit fece7eaf

--- app/src/main/java/com/sportsandhealth/iamkerel/stomachvacuum/SettingsActivity.java
+++ app/src/main/java/com/sportsandhealth/iamkerel/stomachvacuum/SettingsActivity.java
@@ -1,13 +1,7 @@
 package com.sportsandhealth.iamkerel.stomachvacuum;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import androidx.cardview.widget.CardView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SettingsActivity extends Activity {
 
@@ -15,35 +9,5 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.settings_to_home);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goBack();
-            }
-        });
-
-        CardView card0 = (CardView) findViewById(R.id.notificationCard);
-        card0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingsActivity.this, NotificationActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    public void onBackPressed() {
-        goBack();
-    }
-
-    /**
-     * Метод для кнопки назад или кнопки выхода на макете
-     */
-    public void goBack() {
-        Intent intent = new Intent(SettingsActivity.this, ProgramSelection.class);
-        startActivity(intent);
     }
 }
