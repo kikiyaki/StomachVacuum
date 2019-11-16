71c55704dbf1ae8a5fe0cbea95a76345314d5035
kikiyaki <krl9628@gmail.com>
2019-11-16 15:46:48 +0300

    Уведомления без ресивера (2)

--- app/src/main/java/com/sportsandhealth/iamkerel/stomachvacuum/lib/MyNotification.java
+++ app/src/main/java/com/sportsandhealth/iamkerel/stomachvacuum/lib/MyNotification.java
@@ -13,7 +13,7 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.sportsandhealth.iamkerel.stomachvacuum.R;
-import com.sportsandhealth.iamkerel.stomachvacuum.SettingsActivity;
+import com.sportsandhealth.iamkerel.stomachvacuum.ProgramSelection;
 
 import java.util.Date;
 
@@ -26,6 +26,11 @@ public class MyNotification {
      * Контекст из которого выполняется создание уведомления
      */
     private Context context;
+
+    private String channelId = "MY_CHANNEL_ID";
+    private CharSequence name = "StomachVacuumChannel";
+    private String description = "StomachVacuumChannel";
+    private int notificationId = 777;
 
     public MyNotification(Context context) {
         this.context = context;
@@ -40,10 +45,8 @@ public class MyNotification {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "StomachVacuumChannel";
-            String description = "StomachVacuumChannel";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("MY_CHANNEL_ID", name, importance);
+            NotificationChannel channel = new NotificationChannel(channelId, name, importance);
             channel.setDescription(description);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
@@ -62,14 +65,14 @@ public class MyNotification {
         Log.e("QQQ", "Create notification");
 
         // Create an explicit intent for an Activity in your app
-        Intent intent = new Intent(this.context, SettingsActivity.class);
+        Intent intent = new Intent(this.context, ProgramSelection.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this.context, 0, intent, 0);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this.context, "MY_CHANNEL_ID")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this.context, channelId)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
+                .setContentTitle(context.getString(R.string.notification))
+                .setContentText(context.getString(R.string.time_to_training))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 // Set the intent that will fire when the user taps the notification
                 .setContentIntent(pendingIntent)
@@ -79,7 +82,7 @@ public class MyNotification {
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this.context);
 
         // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(7777777, builder.build());
+        notificationManager.notify(notificationId, builder.build());
     }
 
 
