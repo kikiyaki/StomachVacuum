d1ca66ffd55e2c15b4b1951ba280107172be6ea7
kikiyaki <krl9628@gmail.com>
2020-04-14 01:37:41 +0300

    replace banner with native ad in relax activity

--- app/src/main/java/com/sportsandhealth/iamkerel/stomachvacuum/RelaxActivity.java
+++ app/src/main/java/com/sportsandhealth/iamkerel/stomachvacuum/RelaxActivity.java
@@ -9,16 +9,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.sportsandhealth.iamkerel.stomachvacuum.Promo.PromoCode;
-import com.sportsandhealth.iamkerel.stomachvacuum.Promo.PromoCodeUnlock;
+import com.google.android.gms.ads.formats.MediaView;
+import com.google.android.gms.ads.formats.NativeAdOptions;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAdView;
+
 
 public class RelaxActivity extends Activity {
     int level;
@@ -41,11 +47,25 @@ public class RelaxActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relax);
 
+        MobileAds.initialize(this);
 
+        AdLoader adLoader = new AdLoader.Builder(this, getString(R.string.relax_native_ad_unit_id))
+                .forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
+                    @Override
+                    public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
+                        UnifiedNativeAdView unifiedNativeAdView = (UnifiedNativeAdView) getLayoutInflater().inflate(R.layout.native_ad_layout, null);
+                        mapUnifiedNativeAdToLayout(unifiedNativeAd, unifiedNativeAdView);
+
+                        FrameLayout nativeAdLayout = findViewById(R.id.id_native_ad);
+                        nativeAdLayout.removeAllViews();
+                        nativeAdLayout.addView(unifiedNativeAdView);
+                    }
+                })
+                .build();
+        adLoader.loadAd(new AdRequest.Builder().build());
 
         progressBar = (ProgressBar) findViewById(R.id.relax_progress);
         relaxTime = (TextView) findViewById(R.id.relax_time);
@@ -156,5 +176,65 @@ public class RelaxActivity extends Activity {
 
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+    }
+
+    public void mapUnifiedNativeAdToLayout(UnifiedNativeAd adFromGoogle, UnifiedNativeAdView myAdView) {
+        MediaView mediaView = myAdView.findViewById(R.id.ad_media);
+        myAdView.setMediaView(mediaView);
+
+        myAdView.setHeadlineView(myAdView.findViewById(R.id.ad_headline));
+        myAdView.setBodyView(myAdView.findViewById(R.id.ad_body));
+        myAdView.setCallToActionView(myAdView.findViewById(R.id.ad_call_to_action));
+        myAdView.setIconView(myAdView.findViewById(R.id.ad_icon));
+        myAdView.setPriceView(myAdView.findViewById(R.id.ad_price));
+        myAdView.setStarRatingView(myAdView.findViewById(R.id.ad_rating));
+        myAdView.setStoreView(myAdView.findViewById(R.id.ad_store));
+        myAdView.setAdvertiserView(myAdView.findViewById(R.id.ad_advertiser));
+
+        ((TextView) myAdView.getHeadlineView()).setText(adFromGoogle.getHeadline());
+
+        if (adFromGoogle.getBody() == null) {
+            myAdView.getBodyView().setVisibility(View.GONE);
+        } else {
+            ((TextView) myAdView.getBodyView()).setText(adFromGoogle.getBody());
+        }
+
+        if (adFromGoogle.getCallToAction() == null) {
+            myAdView.getCallToActionView().setVisibility(View.GONE);
+        } else {
+            ((Button) myAdView.getCallToActionView()).setText(adFromGoogle.getCallToAction());
+        }
+
+        if (adFromGoogle.getIcon() == null) {
+            myAdView.getIconView().setVisibility(View.GONE);
+        } else {
+            ((ImageView) myAdView.getIconView()).setImageDrawable(adFromGoogle.getIcon().getDrawable());
+        }
+
+        if (adFromGoogle.getPrice() == null) {
+            myAdView.getPriceView().setVisibility(View.GONE);
+        } else {
+            ((TextView) myAdView.getPriceView()).setText(adFromGoogle.getPrice());
+        }
+
+        if (adFromGoogle.getStarRating() == null) {
+            myAdView.getStarRatingView().setVisibility(View.GONE);
+        } else {
+            ((RatingBar) myAdView.getStarRatingView()).setRating(adFromGoogle.getStarRating().floatValue());
+        }
+
+        if (adFromGoogle.getStore() == null) {
+            myAdView.getStoreView().setVisibility(View.GONE);
+        } else {
+            ((TextView) myAdView.getStoreView()).setText(adFromGoogle.getStore());
+        }
+
+        if (adFromGoogle.getAdvertiser() == null) {
+            myAdView.getAdvertiserView().setVisibility(View.GONE);
+        } else {
+            ((TextView) myAdView.getAdvertiserView()).setText(adFromGoogle.getAdvertiser());
+        }
+
+        myAdView.setNativeAd(adFromGoogle);
     }
 }
